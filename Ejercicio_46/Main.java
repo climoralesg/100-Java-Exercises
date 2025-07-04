2f7d9a98fc3f4991b398e71d91c82eff6e101c3f
Claudio Morales <climoralesg@gmail.com>
2025-07-03 20:48:18 -0400

    Ejercicio_46

---
+++ Ejercicio_46/Main.java
@@ -0,0 +1,43 @@
+package Ejercicio_46;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+     public static void diagonalDirecta(int[][] dataMatriz){
+        System.out.println("\n");
+
+        for(int j = 0; j < dataMatriz[0].length ; j++ ){       
+            System.out.println(dataMatriz[j][j]);
+        }
+
+    }
+
+    public static void diagonalInversa(int[][] dataMatriz){
+        System.out.println("\n");
+        
+        for (int j = dataMatriz[0].length - 1, i = 0; j >= 0 && i < dataMatriz[0].length; j--, i++) {
+            System.out.println(dataMatriz[i][j]); // <- ¿Esto es correcto? 
+        }
+
+    }
+    
+    public static void main(String args[]){
+        int[][] matriz = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese los valores de la matriz");
+
+        for(int i = 0; i < matriz.length;i++){
+            for(int j = 0; j < matriz[0].length ; j++ ){
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        diagonalDirecta(matriz);
+        diagonalInversa(matriz);
+         
+    }
+}
