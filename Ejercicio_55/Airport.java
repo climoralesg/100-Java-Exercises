66ccb43485d54503a24f51999f54fa2fe329e3ab
Claudio Morales <climoralesg@gmail.com>
2025-07-22 19:39:29 -0400

    main: add Ejercicio_55

---
+++ Ejercicio_55/Airport.java
@@ -0,0 +1,102 @@
+package Ejercicio_54;
+
+
+import java.time.LocalDate;
+public class Airport {
+
+    public String name;
+    public int yearStart;
+    public int capacity;
+
+    public Address address;
+
+    public Airport(){
+
+    }
+
+    public Airport(String name, String country, String street, int number, String city, int yearStart,
+            int capacity) {
+
+        this.name = name;
+        this.yearStart = yearStart;
+        this.capacity = capacity;
+
+        this.address = new Address(country, street, number, city);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getYearStart() {
+        return yearStart;
+    }
+
+    public void setYearStart(int yearStart) {
+        this.yearStart = yearStart;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getYearsOpen(){
+
+        LocalDate today = LocalDate.now();
+        int yearToday = today.getYear();
+        int yearWorks = yearToday - getYearStart();
+        return yearWorks;
+    }
+
+    public String toString() {
+        return "Aeropuerto [name=" + name + ", country=" + country + ", street=" + street + ", number=" + number
+                + ", city=" + city + ", yearStart=" + yearStart + ", capacity=" + capacity + ", getYearsOpen()="
+                + getYearsOpen() + "]";
+    }
+
+    public Address getAddress(){
+        return this.address;
+    }
+
+}
---
+++ Ejercicio_55/Main.java
@@ -0,0 +1,12 @@
+package Ejercicio_55;
+
+/*
+ * 
+ * Ejercicio 55: Crear clase Dirección
+Define una clase Dirección para almacenar la dirección del Aeropuerto.
+La clase Aeropuerto utilizará un objeto Dirección.
+ */
+
+public class Main {
+    Airport airport = new Airport("Benitez", "Chile", "Calle angosta", 1058, "Santiago", 2002, 10000000);
+}
